171e740d6365da5cd5694e49964a287ee36497c1
Brooks Parrish <parrish.brooks@gmail.com>
2018-05-20 15:13:45 -0400

    Add resume and update some information

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,42 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const less = require('gulp-less');
-const path = require('path');
-const browserSync = require('browser-sync');
-const uglify = require('gulp-uglify');
-const pump = require('pump');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require("gulp");
+const less = require("gulp-less");
+const path = require("path");
+const browserSync = require("browser-sync");
+const uglify = require("gulp-uglify");
+const pump = require("pump");
+const cleanCSS = require("gulp-clean-css");
 
 const reload = browserSync.reload;
 
-gulp.task('less', function() {
-    return gulp.src('./less/**/*.less')
-        .pipe(less({
-            paths: [ path.join(__dirname, 'less', 'includes') ]
-        }))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('./css'))
-        .pipe(reload({ stream:true }));
+gulp.task("less", function() {
+  return gulp
+    .src("./less/**/*.less")
+    .pipe(
+      less({
+        paths: [path.join(__dirname, "less", "includes")]
+      })
+    )
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("./css"))
+    .pipe(reload({ stream: true }));
 });
 
-gulp.task('uglify', function(cb) {
-    pump([
-        gulp.src('scripts/*.js'),
-        uglify(),
-        gulp.dest('js')
-    ], cb);
+gulp.task("uglify", function(cb) {
+  pump([gulp.src("scripts/*.js"), uglify(), gulp.dest("js")], cb);
 });
 
 // watch files for changes and reload
-gulp.task('serve', ['less', 'uglify'], function() {
-    browserSync({
-        server: {
-            baseDir: '.'
-        }
-    });
+gulp.task("serve", ["less", "uglify"], function() {
+  browserSync({
+    server: {
+      baseDir: "."
+    }
+  });
 
-    gulp.watch(['*.html', 'less/*.less', 'scripts/*.js'], ['less', 'uglify', reload]);
+  gulp.watch(
+    ["*.html", "less/*.less", "scripts/*.js"],
+    ["less", "uglify", reload]
+  );
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,22 @@
 $(function() {
-    function loadImages(images) {
-        $(images).each(function(){
-            (new Image()).src = this;
-        });
-    }
-
-    loadImages(['/assets/AUFC.svg', '/assets/Braves.svg', '/assets/THFC.svg', '/assets/UGA.svg']);
-
-    $('.pop').hover(function() {
-        $(this).addClass('popped');
+  function loadImages(images) {
+    $(images).each(function() {
+      new Image().src = this;
     });
+  }
 
-    $('.pop').focus(function() {
-        $(this).addClass('popped');
-    });
+  loadImages([
+    "/assets/AUFC.svg",
+    "/assets/Braves.svg",
+    "/assets/THFC.svg",
+    "/assets/UGA.svg"
+  ]);
+
+  $(".pop").hover(function() {
+    $(this).addClass("popped");
+  });
+
+  $(".pop").focus(function() {
+    $(this).addClass("popped");
+  });
 });
